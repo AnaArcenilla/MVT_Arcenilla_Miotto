2b3b9fdcb81875695fa4d9fa46ddcd645a21509e
Ana <ana.arcenilla@gmail.com>
2022-05-17 09:55:42 -0300

    Se gregan atributos a los familiares

--- app_familiares/models.py
+++ app_familiares/models.py
@@ -7,3 +7,6 @@ class Familiar(models.Model):
     apellido=models.CharField(max_length=30)
     dni=models.BigIntegerField()
     fecha_nacimiento=models.DateField()
+    altura=models.FloatField()
+    color_de_ojos=models.CharField(max_length=30) 
+    ocupacion=models.CharField(max_length=30)
